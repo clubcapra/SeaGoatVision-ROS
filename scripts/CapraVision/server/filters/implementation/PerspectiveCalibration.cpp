d830589318dc945f1539d78a61c5d1e795e1cc55
Yohan <yohtm@hotmail.com>
2015-03-31 00:15:37 -0400

    gui pour la calibraiton de la vision et gestion des parametres

--- scripts/CapraVision/server/filters/implementation/PerspectiveCalibration.cpp
+++ scripts/CapraVision/server/filters/implementation/PerspectiveCalibration.cpp
@@ -8,35 +8,39 @@
 using namespace cv;
 
 
-int topLeftX_ = 0;
-int topLeftY_ = 0;
-int bottomLeftX_ = 0;
-int bottomLeftY_ = 0;
-int topRightX_ = 0;
-int topRightY_ = 0;
-int bottomRightX_ = 0;
-int bottomRightY_ = 0;
+int topLeftX = 0;
+int topLeftY = 0;
+int bottomLeftX = 0;
+int bottomLeftY = 0;
+int topRightX = 0;
+int topRightY = 0;
+int bottomRightX = 0;
+int bottomRightY = 0;
 
-int dx_ = 0;
-int dy_ = 0;
-int zoom_ = 100;
+int translationX = 0;
+int translationY = 0;
+int zoom = 100;
 
 void init() {
-	//rotationX_ = ParameterAsInt("rotationX_", -360, 360, -160);
-	//rotationY_ = ParameterAsInt("rotationY_", -360, 360, 0);
-	zoom_ = ParameterAsInt("Zoom", 0, 500, 100);
-	dx_ = ParameterAsInt("Translation X", -1000, 1000, 0);
-	dy_ = ParameterAsInt("Translation Y", -1000, 1000, 0);
+	//rotationX = ParameterAsInt("rotationX", -360, 360, -160);
+	//rotationY = ParameterAsInt("rotationY", -360, 360, 0);
 
+    topLeftX = ParameterAsInt("topLeftX",-1500,1500,0);
+    topLeftY = ParameterAsInt("topLeftY",-1500,1500,0);
+    bottomLeftX = ParameterAsInt("bottomLeftX",0,1500,202);
+    bottomLeftY = ParameterAsInt("bottomLeftY",0,1500,734);
+    topRightX = ParameterAsInt("topRightX",0,1500,1292);
+    topRightY = ParameterAsInt("topRightY",0,1500,0);
+    bottomRightX = ParameterAsInt("bottomRightX",0,1500,1090);
+    bottomRightY = ParameterAsInt("bottomRightY",0,1500,734);
 
-    topLeftX_ = ParameterAsInt("Top Left X",-1500,1500,-200);
-    topLeftY_ = ParameterAsInt("Top Left TY",-1500,1500,0);
-    bottomLeftX_ = ParameterAsInt("Bottom Left X",0,1500,202);
-    bottomLeftY_ = ParameterAsInt("Bottom Left Y",0,1500,734);
-    topRightX_ = ParameterAsInt("Top Right X",0,1500,1292);
-    topRightY_ = ParameterAsInt("Top Right Y",0,1500,1292);
-    bottomRightX_ = ParameterAsInt("Bottom Right X",0,1500,1090);
-    bottomRightY_ = ParameterAsInt("Bottom Right Y",0,1500,734);
+	translationX = ParameterAsInt("translationX", -1000, 1000, 0);
+	translationY = ParameterAsInt("translationY", -1000, 1000, 0);
+	zoom = ParameterAsInt("zoom", 0, 500, 100);
+}
+
+void configure() {
+	init();
 }
 
 cv::Mat perspective(cv::Mat image){
@@ -51,10 +55,10 @@ cv::Mat perspective(cv::Mat image){
         src_vertices[3] = cv::Point(w,h);
 
         cv::Point2f dst_vertices[4];
-        dst_vertices[0] = cv::Point(topLeftX_, topLeftY_);
-        dst_vertices[1] = cv::Point(bottomLeftX_, bottomLeftY_);
-        dst_vertices[2] = cv::Point(topRightX_, topRightY_);
-        dst_vertices[3] = cv::Point(bottomRightX_, bottomRightY_);
+        dst_vertices[0] = cv::Point(topLeftX, topLeftY);
+        dst_vertices[1] = cv::Point(bottomLeftX, bottomLeftY);
+        dst_vertices[2] = cv::Point(topRightX, topRightY);
+        dst_vertices[3] = cv::Point(bottomRightX, bottomRightY);
 
         cv::Mat warpMatrix = cv::getPerspectiveTransform(src_vertices, dst_vertices);
 
@@ -80,8 +84,8 @@ cv::Mat transform(cv::Mat image){
     // someone wants to use them at some point
 
 	// Rotation matrices around the X axis
-	int rotationX_ = 0;
-	const double alpha = rotationX_ / 180. * 3.1416 / w;
+	int rotationX = 0;
+	const double alpha = rotationX / 180. * 3.1416 / w;
 	Mat RX = (Mat_<double>(4, 4) <<
 		1,          0,           0, 0,
 		0, cos(alpha), -sin(alpha), 0,
@@ -89,8 +93,8 @@ cv::Mat transform(cv::Mat image){
 		0,          0,           0, 1);
 
 	// Rotation matrices around the X axis
-    int rotationY_ = 0;
-	const double beta = rotationY_ / 180. * 3.1416 / h;
+    int rotationY = 0;
+	const double beta = rotationY / 180. * 3.1416 / h;
 	Mat RY = (Mat_<double>(4, 4) <<
 		cos(beta),  0,           sin(beta), 0,
 		0,			1,			 0, 0,
@@ -99,9 +103,9 @@ cv::Mat transform(cv::Mat image){
 
 	// Translation matrix on the Z axis
 	Mat T = (Mat_<double>(4, 4) <<
-		1, 0, 0, 0,
-		0, 1, 0, 0,
-		0, 0, 1, zoom_/100.0f,
+		1, 0, 0, translationX,
+		0, 1, 0, translationY,
+		0, 0, 1, zoom/100.0f,
 		0, 0, 0, 1);
 
 	const double f = 1;
@@ -118,10 +122,6 @@ cv::Mat transform(cv::Mat image){
 	warpPerspective(image, destination, transfo, image.size(), INTER_CUBIC | WARP_INVERSE_MAP);
 
 	return destination;
-}
-
-void configure() {
-	init();
 }
 
 cv::Mat execute(cv::Mat image)
