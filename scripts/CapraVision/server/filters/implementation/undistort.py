681d0d6e46c9827f89a25d96881502342d4b2557
Ibex-1 <robot@clubcapra.com>
2015-05-14 19:05:30 -0400

    calib for half-size image

--- scripts/CapraVision/server/filters/implementation/undistort.py
+++ scripts/CapraVision/server/filters/implementation/undistort.py
@@ -26,15 +26,15 @@ class Undistort:
     def execute(self, image):
     
         distCoeffs = numpy.array([-0.34, 0.085, 0, 0, -0.007])
-        cameraMatrix = numpy.matrix([[630.79035702238025, 0, 645.50000000000000],
-                                     [0, 630.79035702238025, 366.50000000000000],
+        cameraMatrix = numpy.matrix([[630.79035702238025/2, 0, 645.50000000000000/2],
+                                     [0, 630.79035702238025/2, 366.50000000000000/2],
                                      [0,                  0,                  1]])
         size = (1292/2, 734/2)
         
         #technique 2
         newimage = numpy.matrix([])
-        largeCameraMatrix = numpy.matrix([[630.79035702238025, 0, 645.50000000000000],
-                                          [0, 630.79035702238025, 366.50000000000000],
+        largeCameraMatrix = numpy.matrix([[630.79035702238025/2, 0, 645.50000000000000/2],
+                                          [0, 630.79035702238025/2, 366.50000000000000/2],
                                           [0,                  0,                  1]])
         optimalMat, roi = cv2.getOptimalNewCameraMatrix(largeCameraMatrix, distCoeffs, size, 1)
         
